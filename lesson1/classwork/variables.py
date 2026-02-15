0510052ce0da91bcb2e951e2a25fc2c34e8018a6
megabuilder2345 <elliotanselbaer@gmail.com>
2026-02-15 01:58:25 +0000

    finesh lesson 1

---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,14 @@
+x = 5 # interger: whole numbers without a decimal
+y = "Hello world!"
+print(x)
+print(y)
+z=1
+print(z)
+print(x + z)
+favorite_food = "pizzas" # string: text enclosed in quotes
+print(favorite_food)
+print(x, favorite_food) # You can print multiple things on one line by using a comma
+print("my favorite food is", favorite_food)
+print("please stop spamming chat!!!!")
+r= 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482 # float: number with decimal point
+print(r)
