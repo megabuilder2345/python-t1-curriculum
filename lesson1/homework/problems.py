533299c9d629c1b6d8bcf85c927a4e83cabc9b6a
megabuilder2345 <elliotanselbaer@gmail.com>
2026-02-15 03:16:59 +0000

    homework 1

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,33 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
+x=13
+print(x)
 
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
+y=11
+print(y)
 
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
-
+favorite_fruit="blackberry"
+print("My favorite food is a ", favorite_fruit,".", sep="")
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
+country="America"
+city="Seattle"
+print(country)
+print(city)
 
 
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+name="Rosie"
+age=5
+type="bernedoodle"
+print("My dog's name is ", name, ", she is ", age, ", she is a ", type, "!", sep="")
