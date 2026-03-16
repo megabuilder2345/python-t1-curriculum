0fe0c4d947ebcce48f37db213899787b2c2a6c43
MegaBuilder2345 <elliotanselbaer@gmail.com>
2026-03-16 16:35:38 -0700

    fineshed hw5

--- lesson5/homework/problems.py
+++ lesson5/homework/problems.py
@@ -4,15 +4,18 @@ import random
 # Create a list of 3 operating systems.
 # Print the last one using len().
 # Then reverse the list and print it.
-
-
+op=["macOS", "windows","android"]
+print(op[2])
 
 
 # Problem 2
 # Create a list of 4 school subjects.
 # Print the second subject.
 # Then sort them alphabetically and print the result.
-list.sort()
+ss=["maths","english","social studies","science"]
+print(ss[1])
+list.sort(ss)
+print(ss)
 
 
 # Problem 3 
@@ -21,11 +24,17 @@ list.sort()
 # Then find the index of "403" and print it.
 
 
+#we have not learned this
 
-            #ignore!!!!!    # Problem 4 
-                    # Create a list of 2 programming languages.
-                    # Print a random one.
-                  # Then append another language and print the list.
+
+
+
+ #ignore!!!!!  
+
+ # Problem 4 
+ # Create a list of 2 programming languages.
+ # Print a random one.
+ # Then append another language and print the list.
 
 
 
@@ -33,3 +42,7 @@ list.sort()
 # Create a list of 6 passwords.
 # Print the one in the middle using len().
 # Then remove the first password in the list and print it.
+pw=["password", "123", "abc","falkjflaboehllaldkkhalvbih4998ud90a88fa8da8g8h790gs709gf70sdg","MaxCh","minecraftIsAwesome"]
+print(pw[2])
+pw.remove("password")
+print(pw)
