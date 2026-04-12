678a59f6ad14303378b9f6bdf16e2c519ff289ad
MegaBuilder2345 <elliotanselbaer@gmail.com>
2026-04-11 18:02:48 -0700

    fineshed 8.000001

--- lesson8/classwork/local_variable.py
+++ lesson8/classwork/local_variable.py
@@ -1,7 +1,7 @@
 #local variable a variable made inside a funtion, that only exisets inside a funtion
 def adopt_dog():
     pet = "dog" # pet is a local variable
-    print("the pet is:," pet)
+    print("the pet is:", pet)
 
 adopt_dog()
 
